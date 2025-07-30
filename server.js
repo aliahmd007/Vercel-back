6de71d2485e76346d6b517ab4f58eb7a2591f609
Sabri.Gondal09 <aliahmadgondal007@gmail.com>
2025-07-30 10:50:51 +0500

    Upload vercel

--- server.js
+++ server.js
@@ -16,31 +16,31 @@ app.get('/', (req, res) => {
   res.send('API is running.');
 });
 
-// app.post('/send-email', async (req, res) => {
-//   const { name, email, message } = req.body;
+app.post('/send-email', async (req, res) => {
+  const { name, email, message } = req.body;
 
-//   try {
-//     const transporter = nodemailer.createTransport({
-//       service: 'gmail',
-//       auth: {
-//         user: process.env.EMAIL_USER, 
-//         pass: process.env.EMAIL_PASS, 
-//       },
-//     });
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL_USER, 
+        pass: process.env.EMAIL_PASS, 
+      },
+    });
 
-//     await transporter.sendMail({
-//       from: process.env.EMAIL_USER,
-//       to: process.env.EMAIL_USER,
-//       subject: `New message from ${name}`,
-//       text: `Email: ${email}\n\nMessage: ${message}`,
-//     });
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: process.env.EMAIL_USER,
+      subject: `New message from ${name}`,
+      text: `Email: ${email}\n\nMessage: ${message}`,
+    });
 
-//     res.json({ message: 'Email sent successfully!' });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: 'Email failed to send' });
-//   }
-// });
+    res.json({ message: 'Email sent successfully!' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Email failed to send' });
+  }
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on http://localhost:${process.env.PORT}`);
